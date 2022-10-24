19eb50af1e15141c5d916a3d8dd44c44d474aefc
Aaron <aaron155658@gmail.com>
2022-10-24 15:31:31 -0400

    test User, get, getAll, create, update, and delete

--- models/User.js
+++ models/User.js
@@ -1,21 +1,23 @@
 const { Schema, model } = require("mongoose");
 
 // User schema
-const UserSchema = new Schema({
-  username: {
-    type: String,
-    unique: true,
-    require: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    unique: true,
-    require: true,
-    match: [
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      "Please fill a valid email address",
-    ],
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      require: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      require: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
     thoughts: [
       {
         type: Schema.Types.ObjectId,
@@ -29,12 +31,13 @@ const UserSchema = new Schema({
       },
     ],
   },
-  toJSON: {
-    virtuals: true,
-    getters: true,
-  },
-  id: false,
-});
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
 
 UserSchema.virtual("friendCount").get(function () {
   return this.friends.length;
